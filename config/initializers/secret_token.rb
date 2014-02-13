66e98229cbe3885b574856560db0fe8457bdd548
Clay Stewart <claytonmstewart@gmail.com>
2014-02-13 16:01:47 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Podmaker2Bb::Application.config.secret_key_base = 'fc8bdce5c8d1def424dea32e74a8960135200ce4093dfa94c572b92d6c80125053c59b7ad05c66c91a78ffac1100a232a83a9f6757eb89a899be53f9dd71bb41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Podmaker2Bb::Application.config.session_store :cookie_store, key: '_podmaker2-bb_session'
