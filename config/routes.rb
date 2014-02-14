d58d6888f89e4ac7b467cb0a3ad55664518e1530
Taylor Smith <twsmith89@gmail.com>
2014-02-13 22:31:56 -0600

    Create an api route. We're going to namespace API calls as api# and api_. The route is /students because we're going to be getting all the students with this route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Podmaker2Bb::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+
+  scope :api do
+    match '/students', to: 'api#students', via: 'get', as: 'api_students'
+  end
 end
